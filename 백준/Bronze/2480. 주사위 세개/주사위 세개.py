f6d4d05ad857c8bfb6bbceefb97250193f9927e6
Hyeong Chan Kim <siler2474@gmail.com>
2023-01-11 19:29:47 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,9 +1,10 @@
-a, b, c = map(int, input().split())
-if a == b == c:
-    print(10000 + a * 1000)
-elif a == b or a == c:
-    print(1000 + a * 100)
-elif b == c:
-    print(1000 + b * 100)
+l = list(map(int, input().split()))
+s = set(l)
+if len(s) == 1:
+    print(10000 + l[0] * 1000)
+elif len(s) == 3:
+    print(max(l) * 100)
 else:
-    print(max(a, b, c) * 100)
+    for i in s:
+        l.remove(i)
+    print(1000 + l[0] * 100)
