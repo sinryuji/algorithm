cc2151f7025a864f2299398dac06542604fa8ac3
Hyeong Chan Kim <siler2474@gmail.com>
2023-01-11 18:58:46 +0900

    [Bronze V] Title: 사분면 고르기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -1,12 +1,6 @@
 x = int(input())
 y = int(input())
 if x > 0:
-    if y > 0:
-        print(1)
-    else:
-        print(4)
+    print(1 if y > 0 else 4)
 else:
-    if y > 0:
-        print(2)
-    else:
-        print(3)
+    print(2 if y > 0 else 3)
