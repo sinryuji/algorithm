cc948d50ccaed3cadc10c919b4842ca8cba18637
Hyeong Chan Kim <siler2474@gmail.com>
2023-01-16 19:35:30 +0900

    [Bronze V] Title: A+B - 5, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -1,5 +1,6 @@
+import sys
+
 while True:
-    a, b = map(int, input().split())
-    if a == 0 and b == 0:
-        break
+    a, b = map(int, sys.stdin.readline().split())
+    if a == b == 0: break
     print(a + b)
