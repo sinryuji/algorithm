97948534f76954af0dbd07bf68090a0c6aa047f1
Hyeong Chan Kim <siler2474@gmail.com>
2023-01-18 13:39:58 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,8 +1,3 @@
-import sys
-
-nums = []
-for _ in range(9):
-    nums.append(int(sys.stdin.readline()))
+nums = [int(input()) for _ in range(9)]
 max = max(nums)
-print(max)
-print(nums.index(max) + 1)
+print(f"{max}\n{nums.index(max) + 1}")
