ddaacfe30136326d2d6fc63a649a91fb2dd04a5e
Hyeong Chan Kim <siler2474@gmail.com>
2023-01-20 23:02:52 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -3,8 +3,9 @@ import sys
 c = int(sys.stdin.readline())
 for _ in range(c):
     l = list(map(int, sys.stdin.readline().split()))
-    n = l.pop(0)
-    avg = sum(l) / len(l)
+    n = l[0]
+    l = l[1:]
+    avg = sum(l) / n
     cnt = 0
     for i in l:
         if i > avg:
