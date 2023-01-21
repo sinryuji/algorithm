a342f55c4d52df5647d780f61db2550e926b6129
Hyeong Chan Kim <siler2474@gmail.com>
2023-01-21 15:23:32 +0900

    [Silver V] Title: 셀프 넘버, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,12 +1,17 @@
-def solve(num):
-    for n in str(num):
-        num += int(n)
-    return num
-    
-nums = set(range(1, 10000))
-remove = set()
-for num in nums:
-    remove.add(solve(num))
-ret = nums - remove
-for i in sorted(ret):
-    print(i)
+nums = [0 for _ in range(10001)]
+
+def d(n):
+    sum = n
+    while n > 0:
+        sum += n % 10
+        n //= 10
+    return sum
+
+for i in range(1, 10001):
+    n = d(i)
+    if n < 10001:
+        nums[n] = 1
+
+for i in range(1, 10001):
+    if nums[i] == 0:
+        print(i)
