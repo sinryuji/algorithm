31307a7571c5791b048c7a9400bcda7122f436e3
Hyeong Chan Kim <siler2474@gmail.com>
2023-01-27 15:37:11 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,8 +1,5 @@
 croatia = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
 str = input()
-ret = 0
 for c in croatia:
-    ret += str.count(c)
     str = str.replace(c, '.')
-str = str.replace('.', '')
-print(ret + len(str))
+print(len(str))
