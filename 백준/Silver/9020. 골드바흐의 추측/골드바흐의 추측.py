cf6fe204d8d64b8a97058d41f932421f72355863
Hyeong Chan Kim <siler2474@gmail.com>
2023-02-07 18:53:21 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 508 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,17 +1,20 @@
-def isPrime(n):
-    if n == 1:
-        return False
+def getPrimeList(n):
+    n += 1
+    l = [True] * n
+    l[1] = False
     for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+        if l[i]:
+            for j in range(i * 2, n, i):
+                l[j] = False
+    return l
 
+primeList = getPrimeList(10000)
 
 for _ in range(int(input())):
     n = int(input())
     a, b = n // 2, n // 2
     while a > 0:
-        if isPrime(a) and isPrime(b):
+        if primeList[a] and primeList[b]:
             print(a, b)
             break
         else:
