e067c17b37b23813f09d1f38fa3d15321f1e751b
Hyeong Chan Kim <siler2474@gmail.com>
2023-02-07 15:55:06 +0900

    [Silver II] Title: 베르트랑 공준, Time: 636 ms, Memory: 33184 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,15 +1,20 @@
+def getPrimeList(n):
+    n += 1
+    l = [True] * n
+    for i in range(2, int(n ** 0.5) + 1):
+        if l[i]:
+            for j in range(i * 2, n, i):
+                l[j] = False
+    return l
+
+primeList = getPrimeList(123456 * 2)
+
 while True:
     n = int(input())
     if n == 0:
         break
-    m = 2 * n + 1
-    l = [True] * m
-    for i in range(2, int(m ** 0.5) + 1):
-        if l[i]:
-            for j in range(i * 2, m, i):
-                l[j] = False
     cnt = 0
-    for i in range(n + 1, m):
-        if i > 1 and l[i]:
+    for i in range(n + 1, n * 2 + 1):
+        if i > 1 and primeList[i]:
             cnt += 1
     print(cnt)
