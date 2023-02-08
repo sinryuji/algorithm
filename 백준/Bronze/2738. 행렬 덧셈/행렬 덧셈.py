10916681e797d9486d066f780d4218e70f5b83b8
Hyeong Chan Kim <siler2474@gmail.com>
2023-02-08 15:45:27 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -3,12 +3,10 @@ n, m = map(int, input().split())
 a = []
 b = []
 
-for i in range(n):
+for _ in range(n):
     a.append(list(map(int, input().split())))
-for i in range(n):
+for _ in range(n):
     b.append(list(map(int, input().split())))
 for i in range(n):
     ret = [x + y for x, y in zip(a[i], b[i])]
-    for j in ret:
-        print(j, end = ' ')
-    print()
+    print(*ret)
