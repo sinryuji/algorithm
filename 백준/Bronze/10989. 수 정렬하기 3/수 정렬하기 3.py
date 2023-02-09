d106bf7913436b323ee2592dc9e7b040994731c7
Hyeong Chan Kim <siler2474@gmail.com>
2023-02-09 16:21:41 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8788 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline  
+
+n = int(input())
+
+count = [0] * 10001
+
+for _ in range(n):
+    count[int(input())] += 1
+for i in range(len(count)):
+    if count[i] != 0:
+        for j in range(count[i]):
+            print(i)
