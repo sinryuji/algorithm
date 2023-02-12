f3c6c3702db9b9774e77c9fd0bab8f63e228b9c5
Hyeong Chan Kim <siler2474@gmail.com>
2023-02-12 18:42:35 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,7 @@
+arr = list(map(int, input().split()))
+if (sorted(arr) == arr):
+    print("ascending")
+elif (sorted(arr, reverse = True) == arr):
+    print("descending")
+else:
+    print("mixed")
