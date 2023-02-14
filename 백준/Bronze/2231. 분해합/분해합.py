d7fd9ab2ced8e73bfa95fad04c0392a236c13dcc
Hyeong Chan Kim <siler2474@gmail.com>
2023-02-14 19:13:55 +0900

    [Bronze II] Title: 분해합, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,12 +1,14 @@
-n = int(input())
-flag = False
-for i in range(1, n + 1):
-    sum = i
-    for j in str(i):
-        sum += int(j)
-    if sum == n:
+n = input()
+a = int(n) - len(n) * 9
+b = int(n)
+
+if a < 0:
+    a = 0
+
+for i in range(a, b):
+    s = i + sum(map(int, str(i)))
+    if s == int(n):
         print(i)
-        flag = True
         break
-if flag == False:
-    print(0)
+    if i == b - 1:
+        print(0)
