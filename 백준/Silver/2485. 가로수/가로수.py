83c55863901f4120c71385b5669eed6f18d58811
Hyeong Chan Kim <siler2474@gmail.com>
2023-03-11 18:29:30 +0900

    [Silver IV] Title: 가로수, Time: 144 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -0,0 +1,19 @@
+import sys
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+n = int(sys.stdin.readline())
+trees = [int(i) for i in sys.stdin.readlines()]
+gap = []
+for i in range(len(trees) - 1):
+    gap.append(trees[i + 1] - trees[i])
+x = gcd(gap[0], gap[1])
+for i in range(2, len(gap)):
+    x = gcd(x, gap[2])
+ret = 0
+for i in range(len(trees) - 1):
+    ret += (trees[i + 1] - trees[i] - x) // x
+print(ret)
