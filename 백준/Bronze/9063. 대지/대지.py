d8bb1a4f06474026726316d9216d82fc203c1417
Hyeong Chan Kim <siler2474@gmail.com>
2023-03-21 16:41:00 +0900

    [Bronze III] Title: 대지, Time: 1712 ms, Memory: 35196 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.py
@@ -0,0 +1,11 @@
+n = int(input())
+point_x = []
+point_y = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    point_x.append(x)
+    point_y.append(y)
+if n == 1:
+    print(0)
+else:
+    print((max(point_x) - min(point_x)) * (max(point_y) - min(point_y)))
