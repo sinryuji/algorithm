3d5b3b07be3008d4031b4494f211ba0fee446db2
sinryuji <siler2474@gmail.com>
2023-03-27 17:47:06 +0900

    이취코 8-2. 개미 전사

---
+++ 이취코/다이나믹 프로그래밍/8-2. 개미 전사.py
@@ -0,0 +1,11 @@
+n = int(input())
+storage = list(map(int, input().split()))
+
+dp = [0] * (n + 1)
+dp[0] = storage[0]
+dp[1] = max(storage[0], storage[1])
+for i in range(2, n):
+    dp[i] = max(dp[i - 1], dp[i - 2] + storage[i])
+
+print(*dp)
+print(dp[n - 1])
