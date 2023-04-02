104a418af0647fbb7b2d14866cc1734797710756
Hyeong Chan Kim <siler2474@gmail.com>
2023-04-02 11:06:41 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
@@ -5,8 +5,8 @@ n = int(input())
 rainbow = set(["ChongChong"])
 for _ in range(n):
     a, b = input().split()
-    if a in rainbow or a == "ChongChong":
+    if a in rainbow:
         rainbow.add(b)
-    elif b in rainbow or b == "ChongChong":
+    elif b in rainbow:
         rainbow.add(a)
 print(len(rainbow))
