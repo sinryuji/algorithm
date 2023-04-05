dc26d6c87a9b73a4ccf7a1252cb1d9d4e7b2d4b8
Hyeong Chan Kim <siler2474@gmail.com>
2023-04-05 15:24:35 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -3,18 +3,10 @@ input = sys.stdin.readline
 
 t = int(input())
 for _ in range(t):
-  basket = list(input())
-  cnt = 0
-  while len(basket) > 0:
-    c = basket.pop()
-    if c == ')':
-      cnt += 1
-    elif c == '(':
-      cnt -= 1
-    if cnt < 0:
-      print("NO")
-      break
-  if cnt == 0:
-    print("YES")
-  elif cnt > 0:
+  basket = input().rstrip()
+  while '()' in basket:
+        basket = basket.replace('()', '')
+  if len(basket) > 0:
     print("NO")
+  else:
+    print("YES")
