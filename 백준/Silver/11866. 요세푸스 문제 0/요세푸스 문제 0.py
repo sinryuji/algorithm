ad1d7afb67b5227ce72ccf266a98d796742571d5
Hyeong Chan Kim <siler2474@gmail.com>
2023-04-06 17:34:12 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 104 ms, Memory: 34104 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -2,12 +2,10 @@ from collections import deque
 
 n, k = map(int, input().split())
 q = deque([i for i in range(1, n + 1)])
-print('<', end = '')
+ret = []
 while q:
     for _ in range(k - 1):
         q.append(q[0])
         q.popleft()
-    print(q.popleft(), end = '')
-    if q:
-        print(', ', end = '')
-print('>')
+    ret.append(str(q.popleft()))
+print('<', ', '.join(ret), '>', sep = '')
