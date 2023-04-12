f56f118ff1d0cc73885f41cf29f923520ed7bbaf
Hyeong Chan Kim <siler2474@gmail.com>
2023-04-12 13:18:34 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+s = [int(input()) for _ in range(n)]
+dp = [0] * n
+if n <= 2:
+    print(sum(s))
+else:
+    dp[0] = s[0]
+    dp[1] = s[0] + s[1]
+    dp[2] = max(s[1] + s[2], s[0] + s[2])
+    for i in range(3, n):
+        dp[i] = max(dp[i - 3] + s[i - 1] + s[i], dp[i - 2] + s[i])
+    print(dp[n - 1])
