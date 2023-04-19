058289dc37deba2657187e400581f05454ba9288
Hyeong Chan Kim <siler2474@gmail.com>
2023-04-19 22:40:56 +0900

    [Silver IV] Title: 수 찾기, Time: 116 ms, Memory: 50400 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,25 +1,6 @@
-def binary_search(target, data):
-    start = 0
-    end = len(data) - 1
-    
-    while start <= end:
-        mid = (start + end) // 2
-        if data[mid] == target:
-            return mid
-        elif data[mid] < target:
-            start = mid + 1
-        else:
-            end = mid - 1
-    
-    return None
+import sys
 
-n = int(input())
-a = sorted(list(map(int, input().split())))
-m = int(input())
-b = list(map(int, input().split()))
-for i in b:
-    idx = binary_search(i, a)
-    if idx != None:
-        print('1')
-    else:
-        print('0')
+sys.stdin.readline()
+a = set(sys.stdin.readline().split())
+sys.stdin.readline()
+sys.stdout.write('\n'.join('1' if i in a else '0' for i in sys.stdin.readline().split()))
