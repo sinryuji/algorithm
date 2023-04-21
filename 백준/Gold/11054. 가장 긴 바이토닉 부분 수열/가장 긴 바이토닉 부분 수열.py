c66246e09736bc8895ec2ef88a3cdb3a03675657
Hyeong Chan Kim <siler2474@gmail.com>
2023-04-21 17:57:15 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -1,19 +1,38 @@
+def binary_search(target, data):
+    start = 0
+    end = len(data) - 1
+    
+    while start < end:
+        mid = (start + end) // 2
+        if data[mid] >= target:
+            end = mid
+        else:
+            start = mid + 1
+    
+    return start
+
+def lis(data):
+    dp = [1] * n
+    s = [data[0]]
+    
+    for i in range(1, n):
+        if s[-1] < data[i]:
+            s.append(data[i])
+        else:
+            s[binary_search(data[i], s)] = data[i]
+        dp[i] = len(s)
+        
+    return dp
+
 n = int(input())
 a = list(map(int, input().split()))
+ret = 1
 
-increase = [1] * n
-decrease = [1] * n
-result = [0] * n
+increase = lis(a)
+decrease = lis(a[::-1])
+decrease = decrease[::-1]
 
-for i in range(n):
-    for j in range(i):
-        if a[i] > a[j]:
-            increase[i] = max(increase[i], increase[j] + 1)
-for i in range(n - 1, -1, -1):
-    for j in range(i, n):
-        if a[i] > a[j]:
-            decrease[i] = max(decrease[i], decrease[j] + 1)
-    result[i] = increase[i] + decrease[i] - 1
+for i in range(n - 1):
+    ret = max(ret, increase[i] + decrease[i] - 1)
 
-
-print(max(result))
+print(ret)
