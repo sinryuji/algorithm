b0e879d5c3cbdf4a106388dce86523c670eac433
Hyeong Chan Kim <siler2474@gmail.com>
2023-05-01 21:54:46 +0900

    [Gold V] Title: LCS, Time: 220 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -2,13 +2,14 @@ a = input()
 b = input()
 a_len = len(a)
 b_len = len(b)
-dp = [[0] * (b_len + 1) for _ in range(a_len + 1)]
+dp = [0] * max(a_len, b_len)
 
-for i in range(1, a_len + 1):
-    for j in range(1, b_len + 1):
-        if a[i - 1] == b[j - 1]:
-            dp[i][j] = dp[i - 1][j - 1] + 1
-        else:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+for i in range(a_len):
+    n = 0
+    for j in range(b_len):
+        if n < dp[j]:
+            n = dp[j]
+        elif a[i] == b[j]:
+            dp[j] = n + 1
 
-print(dp[-1][-1])
+print(max(dp))
