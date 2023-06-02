bffd89377fd4afd624374f10bf163e78c718d88b
Hyeong Chan Kim <siler2474@gmail.com>
2023-06-02 14:20:09 +0900

    [Silver II] Title: 나무 자르기, Time: 412 ms, Memory: 113900 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,23 +1,18 @@
+import sys
+from collections import Counter
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
-t = list(map(int, input().split()))
+t = Counter(map(int, input().split()))
 
-def solution(t, m): 
-    start = 1
-    end = max(t)
-    while start <= end:
-        mid = (start + end) // 2
-        cuts = 0
-        for i in t:
-            diff = i - mid
-            if diff > 0:
-                cuts += diff
-        if cuts == m:
-            return mid
-        if cuts < m:
-            end = mid - 1
-        else:
-            start = mid + 1
-
-    return end
+start = 1
+end = max(t)
+while start <= end:
+    mid = (start + end) // 2
+    cuts = sum((h - mid) * i for h, i in t.items() if h > mid)
+    if cuts < m:
+        end = mid - 1
+    else:
+        start = mid + 1
         
-print(solution(t, m))
+print(end)
