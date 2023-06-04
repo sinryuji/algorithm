e34a8104ca572e6b2912a5b5084b09bcf67de966
Hyeong Chan Kim <siler2474@gmail.com>
2023-06-04 22:12:03 +0900

    [Gold II] Title: K번째 수, Time: 1092 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N, K = int(input()), int(input())
+start, end = 1, K
+
+while start <= end:
+    mid = (start + end) // 2
+    
+    tmp = 0
+    for i in range(1, N + 1):
+        tmp += min(mid // i, N)
+    
+    if tmp >= K:
+        answer = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+        
+print(answer)
