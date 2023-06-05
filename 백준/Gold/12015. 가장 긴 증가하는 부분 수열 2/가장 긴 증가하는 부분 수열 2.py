09a4db0ebc1be74b7f66651f40a977f98bb9c922
Hyeong Chan Kim <siler2474@gmail.com>
2023-06-05 22:04:20 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 852 ms, Memory: 157628 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -1,26 +1,15 @@
+import sys
+input = sys.stdin.readline
+from bisect import bisect_left
+
 n = int(input())
 nums = list(map(int, input().split()))
 s = [nums[0]]
-
-def binary_search(target, data):
-    start = 0
-    end = len(data) - 1
-    answer = 0
-    
-    while start <= end:
-        mid = (start + end) // 2
-        if data[mid] >= target:
-            answer = mid
-            end = mid - 1
-        else:
-            start = mid + 1
-    
-    return answer
 
 for i in nums[1:]:
     if s[-1] < i:
         s.append(i)
     else:
-        s[binary_search(i, s)] = i
+        s[bisect_left(s, i)] = i
         
 print(len(s))
