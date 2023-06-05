437f7c838243d9c3394fc2cf42b62ec7b235a19f
Hyeong Chan Kim <siler2474@gmail.com>
2023-06-05 21:49:57 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 1996 ms, Memory: 155588 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -5,14 +5,17 @@ s = [nums[0]]
 def binary_search(target, data):
     start = 0
     end = len(data) - 1
+    answer = 0
     
-    while start < end:
+    while start <= end:
         mid = (start + end) // 2
         if data[mid] >= target:
-            end = mid
+            answer = mid
+            end = mid - 1
         else:
             start = mid + 1
-    return start
+    
+    return answer
 
 for i in nums[1:]:
     if s[-1] < i:
