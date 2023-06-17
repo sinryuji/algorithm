5a0096a320ea58175884ea024659073d83dfb5cf
Hyeong Chan Kim <siler2474@gmail.com>
2023-06-17 16:13:34 +0900

    [Silver V] Title: 집합, Time: 3380 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,29 +1,22 @@
 import sys
+input = sys.stdin.readline
 
-m = int(sys.stdin.readline())
-S = set()
+m = int(input())
+s = set()
+s_all = set([str(i) for i in range(1, 21)])
 
 for _ in range(m):
-    temp = sys.stdin.readline().strip().split()
-    
-    if len(temp) == 1:
-        if temp[0] == "all":
-            S = set([i for i in range(1, 21)])
-        else:
-            S = set()
-    
-    else:
-        func, x = temp[0], temp[1]
-        x = int(x)
-
-        if func == "add":
-            S.add(x)
-        elif func == "remove":
-            S.discard(x)
-        elif func == "check":
-            print(1 if x in S else 0)
-        elif func == "toggle":
-            if x in S:
-                S.discard(x)
-            else:
-                S.add(x)
+    cmd = list(input().split())
+    if cmd[0] == "all":
+        s = s_all
+    elif cmd[0] == "empty":
+        s = set()
+    elif cmd[0] == "add":
+        s.add(cmd[1])
+    elif cmd[0] == "remove":
+        s.discard(cmd[1])
+    elif cmd[0] == "check":
+        print(1 if cmd[1] in s else 0)
+    elif cmd[0] == "toggle":
+        if cmd[1] in s: s.discard(cmd[1])
+        else: s.add(cmd[1])
