c9f96894289c7956e561476ced607091b0816585
Hyeong Chan Kim <siler2474@gmail.com>
2023-08-11 21:23:42 +0900

    [Gold V] Title: 토마토, Time: 1356 ms, Memory: 114956 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-def bfs(queue):
+def bfs(queue, cnt):
     d = 0
     while queue:
         x, y, d = queue.popleft()
@@ -17,21 +17,21 @@ def bfs(queue):
             if graph[ny][nx] == 0:
                 graph[ny][nx] = 1
                 queue.append([nx, ny, d + 1])
+                cnt -= 1
+    if cnt > 0:
+        return -1
     return d
 
 m, n = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-
 queue = deque()
+cnt = 0
+
 for y in range(n):
     for x in range(m):
         if graph[y][x] == 1:
             queue.append([x, y, 0])
+        elif graph[y][x] == 0:
+            cnt += 1
 
-answer = bfs(queue)
-for y in range(n):
-    for x in range(m):
-        if graph[y][x] == 0:
-            print(-1)
-            exit()
-print(answer)
+print(bfs(queue, cnt))
