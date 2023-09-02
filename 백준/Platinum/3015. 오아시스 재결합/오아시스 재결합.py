f3d838f960fdd3892361b1d232d23f9681490c32
Hyeong Chan Kim <siler2474@gmail.com>
2023-09-02 12:07:22 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 464 ms, Memory: 50684 KB -BaekjoonHub

---
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+line = [int(input()) for _ in range(n)]
+stack = []
+result = 0
+
+for i in line:
+    while stack and stack[-1][0] < i:
+        result += stack.pop()[1]
+        
+    if not stack:
+        stack.append((i, 1))
+        continue
+    
+    if stack[-1][0] == i:
+        cnt = stack.pop()[1]
+        result += cnt
+        if stack: result += 1
+        stack.append((i, cnt + 1))
+    else:
+        stack.append((i, 1))
+        result += 1
+        
+print(result)
