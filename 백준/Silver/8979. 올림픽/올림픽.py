003ed1d81cb14042baad226fc3c1d5376be9181b
Hyeong Chan Kim <siler2474@gmail.com>
2023-10-07 16:18:26 +0900

    [Silver V] Title: 올림픽, Time: 84 ms, Memory: 31256 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,22 +1,12 @@
 N, K = map(int, input().split())
-medals = dict()
-for _ in range(N):
-    n, g, s, b = map(int, input().split())
-    medals[n] = [g, s, b]
 
-sorted_medals = sorted(medals.items(), key=lambda item: item[1])
+medals = [list(map(int, input().split())) for _ in range(N)]
+    
+medals.sort(key = lambda x : (x[1], x[2], x[3]), reverse=True)
 
-answer = [0] * (N + 1)
-rank = 0
-tmp = 0
-prev = []
-for key, val in sorted_medals:
-    if val != prev:
-        rank += tmp + 1
-        tmp = 0
-    else:
-        tmp += 1
-    answer[key] = rank
-    prev = val
+idx = [medals[i][0] for i in range(N)].index(K)
 
-print(answer[K])
+for i in range(N):
+    if medals[idx][1:] == medals[i][1:]:
+        print(i+1)
+        break
