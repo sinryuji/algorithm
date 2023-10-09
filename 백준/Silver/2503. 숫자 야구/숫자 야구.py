c5ff2395b5fad9598eef7941ec3341e9634855a2
Hyeong Chan Kim <siler2474@gmail.com>
2023-10-09 23:44:39 +0900

    [Silver III] Title: 숫자 야구, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -0,0 +1,23 @@
+from itertools import permutations
+
+N = int(input())
+data = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+nums = list(permutations(data, 3))
+
+for _ in range(N):
+    n, s, b = map(int, input().split())
+    n = list(str(n))
+    rmcnt = 0
+    for i in range(len(nums)):
+        strike, ball = 0, 0
+        i -= rmcnt
+        for j in range(3):
+            if nums[i][j] == n[j]:
+                strike += 1
+            elif n[j] in nums[i]:
+                ball += 1
+        if strike != s or ball != b:
+            nums.remove(nums[i])
+            rmcnt += 1
+            
+print(len(nums))
