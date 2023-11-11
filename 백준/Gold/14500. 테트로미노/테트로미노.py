5357ad5b2dc1258e17fed4e8713ae1c441070fc0
Hyeong Chan Kim <siler2474@gmail.com>
2023-11-11 18:10:23 +0900

    [Gold IV] Title: 테트로미노, Time: 200 ms, Memory: 36420 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,40 +1,37 @@
 N, M = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(N)]
+d = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+max_ = max(map(max, graph))
+answer = 0
 
-figures = [
-    [[0, 0], [1, 0], [2, 0], [3, 0]],
-    [[0, 0], [0, 1], [0, 2], [0, 3]],
-    [[0, 0], [1, 0], [0, 1], [1, 1]],
-    [[0, 0], [0, 1], [0, 2], [1, 2]],
-    [[0, 0], [1, 0], [2, 0], [0, 1]],
-    [[0, 0], [1, 0], [1, 1], [1, 2]],
-    [[2, 0], [0, 1], [1, 1], [2, 1]],
-    [[0, 2], [1, 0], [1, 1], [1, 2]],
-    [[0, 0], [0, 1], [1, 1], [2, 1]],
-    [[0, 0], [0, 1], [0, 2], [1, 0]],
-    [[0, 0], [1, 0], [2, 0], [2, 1]],
-    [[1, 0], [1, 1], [0, 1], [0, 2]],
-    [[0, 0], [1, 0], [1, 1], [2, 1]],
-    [[0, 0], [0, 1], [1, 1], [1, 2]],
-    [[1, 0], [2, 0], [0, 1], [1, 1]],
-    [[0, 0], [1, 0], [2, 0], [1, 1]],
-    [[0, 1], [1, 0], [1, 1], [1, 2]],
-    [[1, 0], [0, 1], [1, 1], [2, 1]],
-    [[0, 0], [0, 1], [0, 2], [1, 1]]
-]
+def dfs(x, y, cnt, sum_):
+    global answer
 
-max_ = 0
+    if answer > sum_ + max_ * (4 - cnt):
+        return
+    if cnt == 4:
+        answer = max(answer, sum_)
+        return
 
-for figure in figures:
-    for x in range(M):
-        for y in range(N):
-            sum_ = 0
-            for coord in figure:
-                nx = coord[0] + x
-                ny = coord[1] + y
-                if nx >= M or ny >= N:
-                    break
-                sum_ += graph[ny][nx]
-            max_ = max(max_, sum_)
+    for dx, dy in d:
+        nx = dx + x
+        ny = dy + y
+        if nx < 0 or nx >= M or ny < 0 or ny >= N or graph[ny][nx] == 0:
+            continue
+        t = graph[ny][nx]
+        if cnt == 2:
+            graph[ny][nx] = 0
+            dfs(x, y, cnt + 1, sum_ + t)
+            graph[ny][nx] = t
+        graph[ny][nx] = 0
+        dfs(nx, ny, cnt + 1, sum_ + t)
+        graph[ny][nx] = t
 
-print(max_)
+for x in range(M):
+    for y in range(N):
+        t = graph[y][x]
+        graph[y][x] = 0
+        dfs(x, y, 1, t)
+        graph[y][x] = t
+
+print(answer)
