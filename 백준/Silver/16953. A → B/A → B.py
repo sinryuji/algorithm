ed14f351ecd6e9a7f8465a1a6b4a122c4b5bdbf9
Hyeong Chan Kim <siler2474@gmail.com>
2023-12-20 00:55:07 +0900

    [Silver II] Title: A → B, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,22 +1,14 @@
-from collections import deque
-
 A, B = map(int, input().split())
 cnt = 1
-queue = deque([[A, cnt]])
-flag = False
 
-while queue:
-    n, cnt = queue.popleft()
-    if n == B:
-        flag = True
-        print(cnt)
-        break
-    mul = n * 2
-    one = n * 10 + 1
-    if mul <= B:
-        queue.append([mul, cnt + 1])
-    if one <= B:
-        queue.append([one, cnt + 1])
+while B > A:
+    if B % 10 == 1:
+        B //= 10
+    else:
+        B /= 2
+    cnt += 1
 
-if not flag:
+if A == B:
+    print(cnt)
+else:
     print(-1)
