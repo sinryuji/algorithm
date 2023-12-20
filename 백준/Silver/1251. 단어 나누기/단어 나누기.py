401d5bb93c955e885779f79b3a364e4a2e084d87
Hyeong Chan Kim <siler2474@gmail.com>
2023-12-21 00:00:58 +0900

    [Silver V] Title: 단어 나누기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.py
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -1,18 +1,14 @@
-word = list(input())
-answer = []
-tmp = []
+word = input()
+word_len = len(word)
+answer = "z" * word_len
 
-for i in range(1, len(word) - 1):
-    for j in range(i + 1, len(word) ):
-        a = word[:i]
-        b = word[i:j]
-        c = word[j:]
-        a.reverse()
-        b.reverse()
-        c.reverse()
-        tmp.append(a + b + c)
+for i in range(1, word_len - 1):
+    for j in range(i + 1, word_len):
+        first = word[0:i]
+        second = word[i:j]
+        third = word[j:]
+        tmp = first[::-1] + second[::-1] + third[::-1]
+        if answer > tmp:
+            answer = tmp
 
-for a in tmp:
-    answer.append(''.join(a))
-
-print(sorted(answer)[0])
+print(answer)
