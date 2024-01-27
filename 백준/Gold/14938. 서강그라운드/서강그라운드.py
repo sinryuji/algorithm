59ea3e2d97a84efe19b0e00c3e9df79cfe90f6f5
Hyeong Chan Kim <siler2474@gmail.com>
2024-01-27 20:49:20 +0900

    [Gold IV] Title: 서강그라운드, Time: 48 ms, Memory: 33212 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.py
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.py
@@ -1,32 +1,45 @@
 import sys
+import heapq
 input = sys.stdin.readline
 INF = int(1e9)
 
+def dijkstra(start):
+    global n
+    global m
+
+    distance = [INF] * n
+    distance[start] = 0
+    queue = []
+    queue.append((start, distance[start]))
+
+    while queue:
+        current, dist = heapq.heappop(queue)
+        if distance[current] < dist:
+            continue
+        for next_, next_dist in graph[current]:
+            cost = dist + next_dist
+            if distance[next_] > cost:
+                distance[next_] = cost
+                heapq.heappush(queue, (next_, cost))
+
+    result = 0
+    for i in range(n):
+        if distance[i] <= m:
+            result += items[i]
+
+    return result
+
 n, m, r = map(int, input().split())
 items = list(map(int, input().split()))
-dist = [[INF] * n for _ in range(n)]
+graph = [[] for _ in range(n)]
 
 for _ in range(r):
     a, b, l = map(int, input().split())
-    dist[a-1][b-1] = l
-    dist[b-1][a-1] = l
-
-for i in range(n):
-    for j in range(n):
-        if i == j:
-            dist[i][j] = 0
-
-for k in range(n):
-    for i in range(n):
-        for j in range(n):
-            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+    graph[a-1].append((b-1, l))
+    graph[b-1].append((a-1, l))
 
 answer = 0
 for i in range(n):
-    result = 0
-    for j in range(n):
-        if dist[i][j] <= m:
-            result += items[j]
-    answer = max(answer, result)
+    answer = max(answer, dijkstra(i))
 
 print(answer)
