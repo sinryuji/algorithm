c402bdd6bebe3a6382ae1a9ab4f19ff2eb9a7ac0
Hyeong Chan Kim <siler2474@gmail.com>
2024-01-30 10:09:14 +0900

    [Gold III] Title: 치즈, Time: 68 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -5,27 +5,26 @@ input = sys.stdin.readline
 def main():
     N, M = map(int, input().split())
     d = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-    board = [list(map(int, input().split())) for _ in range(N)]
+    margin = [[-1] * (M + 2)]
+    board = [[-1] + list(map(int, input().split())) + [-1] for _ in range(N)]
+    board = margin + board + margin
     
-    q, nq, ans = deque([(0, 0)]), deque(), 0
+    q, nq, ans = deque([(1, 1)]), deque(), 0
     
     while q or nq:
         if not q:
             q, nq = nq, deque()
             ans += 1
         r, c = q.popleft()
-        for i in range(4):
-            nr = r + d[i][0]
-            nc = c + d[i][1]
-            if 0 <= nr < N and 0 <= nc < M:
-                if not board[nr][nc]:
+        for dr, dc in d:
+            if board[(nr:=r+dr)][(nc:=c+dc)] > 0:
+                board[nr][nc] += 1
+                if board[nr][nc] > 2:
                     board[nr][nc] = -1
-                    q.append((nr, nc))
-                if board[nr][nc] > 0:
-                    board[nr][nc] += 1
-                    if board[nr][nc] > 2:
-                        board[nr][nc] = -1
-                        nq.append((nr, nc))
+                    nq.append((nr, nc))
+            if not board[nr][nc]:
+                board[nr][nc] = -1
+                q.append((nr, nc))
 
     print(ans)
 
