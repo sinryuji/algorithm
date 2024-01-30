99e4b52622a012a6cbf89fb65d624db80e8892e6
Hyeong Chan Kim <siler2474@gmail.com>
2024-01-30 09:55:29 +0900

    [Gold III] Title: 치즈, Time: 84 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -2,50 +2,28 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-def bfs(N, M):
-    q = deque([(0, 0)])
-    cnt = 0
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < M and 0 <= ny < N and board[ny][nx] == 0:
-                q.append((nx, ny))
-                board[ny][nx] = 2
-                cnt += 1
-    return cnt != N * M
-
-def find_exposed(N, M):
-    for y in range(N):
-        for x in range(M):
-            if board[y][x] == 1:
-                cnt = 0
-                for i in range(4):
-                    nx = x + dx[i]
-                    ny = y + dy[i]
-                    if board[ny][nx] == 2:
-                        cnt += 1
-                if cnt >= 2:
-                    board[y][x] = 3
-
-def remove_exposed(N, M):
-    for y in range(N):
-        for x in range(M):
-            if 2 <= board[y][x] <= 3:
-                board[y][x] = 0
-
 N, M = map(int, input().split())
-board = []
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-for _ in range(N):
-    board.append(list(map(int, input().split())))
+d = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+board = [list(map(int, input().split())) for _ in range(N)]
 
-answer = 0
-while bfs(N, M):
-    find_exposed(N, M)
-    remove_exposed(N, M)
-    answer += 1
+q, nq, ans = deque([(0, 0)]), deque(), 0
 
-print(answer)
+while q or nq:
+    if not q:
+        q, nq = nq, deque()
+        ans += 1
+    r, c = q.popleft()
+    for i in range(4):
+        nr = r + d[i][0]
+        nc = c + d[i][1]
+        if 0 <= nr < N and 0 <= nc < M:
+            if not board[nr][nc]:
+                board[nr][nc] = -1
+                q.append((nr, nc))
+            if board[nr][nc] > 0:
+                board[nr][nc] += 1
+                if board[nr][nc] > 2:
+                    board[nr][nc] = -1
+                    nq.append((nr, nc))
+
+print(ans)
