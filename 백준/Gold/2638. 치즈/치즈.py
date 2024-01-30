2d899c22cf6ffeb970d2d9cd8caa73254d1da2f8
Hyeong Chan Kim <siler2474@gmail.com>
2024-01-30 10:07:22 +0900

    [Gold III] Title: 치즈, Time: 72 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -2,28 +2,32 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-d = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-board = [list(map(int, input().split())) for _ in range(N)]
-
-q, nq, ans = deque([(0, 0)]), deque(), 0
-
-while q or nq:
-    if not q:
-        q, nq = nq, deque()
-        ans += 1
-    r, c = q.popleft()
-    for i in range(4):
-        nr = r + d[i][0]
-        nc = c + d[i][1]
-        if 0 <= nr < N and 0 <= nc < M:
-            if not board[nr][nc]:
-                board[nr][nc] = -1
-                q.append((nr, nc))
-            if board[nr][nc] > 0:
-                board[nr][nc] += 1
-                if board[nr][nc] > 2:
+def main():
+    N, M = map(int, input().split())
+    d = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    board = [list(map(int, input().split())) for _ in range(N)]
+    
+    q, nq, ans = deque([(0, 0)]), deque(), 0
+    
+    while q or nq:
+        if not q:
+            q, nq = nq, deque()
+            ans += 1
+        r, c = q.popleft()
+        for i in range(4):
+            nr = r + d[i][0]
+            nc = c + d[i][1]
+            if 0 <= nr < N and 0 <= nc < M:
+                if not board[nr][nc]:
                     board[nr][nc] = -1
-                    nq.append((nr, nc))
+                    q.append((nr, nc))
+                if board[nr][nc] > 0:
+                    board[nr][nc] += 1
+                    if board[nr][nc] > 2:
+                        board[nr][nc] = -1
+                        nq.append((nr, nc))
 
-print(ans)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
