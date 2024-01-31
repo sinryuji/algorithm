a7fefdb038444f0c4a79ef87c11e7f1b6c58617e
Hyeong Chan Kim <siler2474@gmail.com>
2024-01-31 17:24:57 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 300 ms, Memory: 57396 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -1,44 +1,39 @@
 import sys
-from collections import defaultdict
 import heapq
+input = sys.stdin.readline
 INF = int(1e9)
 
-n = int(sys.stdin.readline().rstrip())
-m = int(sys.stdin.readline().rstrip())
-graph = defaultdict(list)
-for _ in range(m):
-    a, b, c = map(int, sys.stdin.readline().rstrip().split())
-    graph[a].append((b, c))
-start, end = map(int, sys.stdin.readline().rstrip().split())
-
-dist = [INF] * (n+1)
-prev_node = [0] * (n+1)
-
 def dijkstra(start):
-    q = []
-    heapq.heappush(q, (0, start))
-    dist[start] = 0
-    while q:
-        weight, node = heapq.heappop(q)
-        if dist[node] < weight:
+    heap = [(start, distance[start])]
+    while heap:
+        curr, dist = heapq.heappop(heap)
+        if distance[curr] < dist:
             continue
-        for adj_node, adj_weight in graph[node]:
-            cost = weight + adj_weight
-            if cost < dist[adj_node]:
-                dist[adj_node] = cost
-                prev_node[adj_node] = node
-                heapq.heappush(q, (cost, adj_node))
+        for n, n_dist in graph[curr]:
+            if distance[n] > dist + n_dist:
+                distance[n] = dist + n_dist
+                prev_node[n] = curr
+                heapq.heappush(heap, (n, distance[n]))
+
+n, m = int(input()), int(input())
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+start, end = map(int, input().split())
+
+distance = [INF] * (n + 1)
+distance[start] = 0
+prev_node = [0] * (n + 1)
+path = [end]
 
 dijkstra(start)
-print(dist[end])
-
-path = [end]
 now = end
 while now != start:
     now = prev_node[now]
     path.append(now)
-
 path.reverse()
 
+print(distance[end])
 print(len(path))
-print(' '.join(map(str, path)))
+print(*path)
