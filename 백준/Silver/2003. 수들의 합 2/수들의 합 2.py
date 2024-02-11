ad534fb97631ab425cf051df094a6a4dd03e7c53
Hyeong Chan Kim <siler2474@gmail.com>
2024-02-11 18:09:25 +0900

    [Silver IV] Title: 수들의 합 2, Time: 476 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -4,16 +4,16 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 nums = list(map(int, input().split()))
 
-p_sum = [0]
-s = 0
-for num in nums:
-    s += num
-    p_sum.append(s)
-
+s, e = 0, 1
 ans = 0
-for i in range(N+1):
-    for j in range(i):
-        if p_sum[i] - p_sum[j] == M:
-            ans += 1
+while e <= N:
+    sum_ = sum(nums[s:e])
+    if sum_ == M:
+        ans += 1
+        e += 1
+    elif sum_ < M:
+        e += 1
+    else:
+        s += 1
 
 print(ans)
