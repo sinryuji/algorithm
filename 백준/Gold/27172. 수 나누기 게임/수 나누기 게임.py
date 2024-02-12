48a21f33fa562b17fca052c4226f0d2e49c3c441
Hyeong Chan Kim <siler2474@gmail.com>
2024-02-12 18:07:28 +0900

    [Gold V] Title: 수 나누기 게임, Time: 676 ms, Memory: 53264 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
@@ -1,24 +1,20 @@
-import sys
-input = sys.stdin.readline
-
-N = int(input())
-nums = []
-ans = dict()
-max_num = 0
-
-for i, num in enumerate([*map(int, input().split())]):
-    max_num = max(max_num, num)
-    nums.append((i, num))
-    ans[num] = 0
-
-nums.sort(key=lambda x : x[1])
-
-for i in range(N):
-    pos, num = nums[i]
-    for target in range(num*2, max_num+1, num):
-        if target in ans:
-            ans[num] += 1
-            ans[target] -= 1
-
-for value in ans.values():
-    print(value, end=' ')
+from sys import stdin
+def main():
+    input = stdin.readline
+    n = int(input())
+    *x, = map(int, input().split())
+    max_ = max(x)+1
+    a = [False]*max_
+    for xi in x:
+        a[xi] = True
+    ans = [0]*max_
+    for xi in x:
+        for i in range(xi*2, max_, xi):
+            if a[i]:
+                ans[xi] += 1
+                ans[i] -= 1
+    tmp = ''
+    for xi in x:
+        tmp += str(ans[xi])+' '
+    print(tmp)
+main()
