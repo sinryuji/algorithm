781048aa0a2ceed721e969b2306aa3b2a999d731
Hyeong Chan Kim <siler2474@gmail.com>
2024-02-16 17:46:25 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 280 ms, Memory: 49216 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+V, E = map(int, input().split())
+edges = []
+for _ in range(E):
+    edges.append(tuple(map(int, input().split())))
+
+parent = [i for i in range(V + 1)]
+edges.sort(key=lambda x : x[2])
+ans = 0
+
+for a, b, dist in edges:
+    if find(a) == find(b):
+        continue
+    ans += dist
+    union(a, b)
+
+print(ans)
