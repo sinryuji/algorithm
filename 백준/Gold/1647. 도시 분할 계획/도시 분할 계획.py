1a86240c4a28d99732608098ab85169825355985
Hyeong Chan Kim <siler2474@gmail.com>
2024-02-26 17:55:52 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2156 ms, Memory: 208396 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,33 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def solve():
+    def find(x):
+        if parent[x] != x:
+            parent[x] = find(parent[x])
+        return parent[x]
+
+    N, M = map(int, input().split())
+    edges = [tuple(map(int, input().split())) for _ in range(M)]
+    edges.sort(key=lambda x: x[2])
+    parent = [i for i in range(N + 1)]
+    sum_ = 0
+    max_ = 0
+
+    for a, b, dist in edges:
+        a, b = find(a), find(b)
+        if a != b:
+            if a < b:
+                parent[a] = b
+            else:
+                parent[b] = a
+            sum_ += dist
+            max_ = max(max_, dist)
+
+    print(sum_ - max_)
+
+
+if __name__ == '__main__':
+    solve()
