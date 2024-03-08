fa008a94c43be4183180efc3201c43f874b53077
Hyeong Chan Kim <siler2474@gmail.com>
2024-03-08 18:04:12 +0900

    [Gold III] Title: ACM Craft, Time: 688 ms, Memory: 34320 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -1,43 +1,30 @@
 import sys
-from collections import deque
-
+sys.setrecursionlimit(int(1e6))
 input = sys.stdin.readline
 
 
-def topology_sort(N, indegree, dp, seq, times):
-    q = deque()
-
-    for i in range(1, N + 1):
-        if indegree[i] == 0:
-            q.append(i)
-            dp[i] = times[i]
-
-    while q:
-        n = q.popleft()
-        for i in seq[n]:
-            indegree[i] -= 1
-            dp[i] = max(dp[n] + times[i], dp[i])
-            if indegree[i] == 0:
-                q.append(i)
-
-    return dp
+def dfs(n, times, graph, dp):
+    if dp[n] != -1:
+        return dp[n]
+    max_ = 0
+    for x in graph[n]:
+        r = dfs(x, times, graph, dp)
+        if r > max_:
+            max_ = r
+    dp[n] = max_ + times[n]
+    return dp[n]
 
 def solve():
     N, K = map(int, input().split())
     times = [0] + list(map(int, input().split()))
-    seq = [[] for _ in range(N + 1)]
-    indegree = [0] * (N + 1)
-    dp = [0] * (N + 1)
+    graph = [[] for _ in range(N + 1)]
+    dp = [-1] * (N + 1)
     for _ in range(K):
         a, b = map(int, input().split())
-        seq[a].append(b)
-        indegree[b] += 1
+        graph[b].append(a)
     W = int(input())
 
-    dp = topology_sort(N, indegree, dp, seq, times)
-
-    print(dp[W])
-
+    print(dfs(W, times, graph, dp))
 
 for _ in range(int(input())):
     solve()
