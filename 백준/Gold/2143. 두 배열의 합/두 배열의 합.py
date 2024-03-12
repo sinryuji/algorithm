6cc03c00e29097e972b677d0f9478557a3f08ee9
Hyeong Chan Kim <siler2474@gmail.com>
2024-03-12 17:52:05 +0900

    [Gold III] Title: 두 배열의 합, Time: 348 ms, Memory: 73108 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
@@ -2,56 +2,29 @@ import sys
 
 input = sys.stdin.readline
 
-
-def bisection_right(array, target):
-    left, right = 0, len(array) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if array[mid] > target:
-            right = mid - 1
-        else:
-            left = mid + 1
-    return right
-
-
-def bisection_left(array, target):
-    left, right = 0, len(array) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if array[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return left
-
-
-
 T = int(input())
 n = int(input())
 A = list(map(int, input().split()))
 m = int(input())
 B = list(map(int, input().split()))
 
-psa, psb = [], []
+psa = {}
 for i in range(n):
-    s = A[i]
-    psa.append(s)
-    for j in range(i + 1, n):
+    s = 0
+    for j in range(i, n):
         s += A[j]
-        psa.append(s)
-for i in range(m):
-    s = B[i]
-    psb.append(s)
-    for j in range(i + 1, m):
-        s += B[j]
-        psb.append(s)
+        if s in psa:
+            psa[s] += 1
+        else:
+            psa[s] = 1
 
-psa.sort()
-psb.sort()
+psb = {}
 ans = 0
-for i in range(len(psa)):
-    l = bisection_left(psb, T - psa[i])
-    r = bisection_right(psb, T - psa[i])
-    ans += r - l + 1
-
+for i in range(m):
+    s = 0
+    for j in range(i, m):
+        s += B[j]
+        if T - s in psa:
+            ans += psa[T - s]
+            
 print(ans)
