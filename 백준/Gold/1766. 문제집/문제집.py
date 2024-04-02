f82216d2ce25c49b0cfc751433494c17d17dc8e9
Hyeong Chan Kim <siler2474@gmail.com>
2024-04-02 16:30:11 +0900

    [Gold II] Title: 문제집, Time: 164 ms, Memory: 40632 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.py
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -3,29 +3,33 @@ import heapq
 
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-graph = [[] for _ in range(N + 1)]
-indegree = [0] * (N + 1)
+def solve():
+    N, M = map(int, input().split())
+    graph = [[] for _ in range(N + 1)]
+    indegree = [0] * (N + 1)
 
-for _ in range(M):
-    A, B = map(int, input().split())
-    graph[A].append(B)
-    indegree[B] += 1
+    for _ in range(M):
+        A, B = map(int, input().split())
+        graph[A].append(B)
+        indegree[B] += 1
 
-heap = []
+    heap = []
 
-for i in range(1, N + 1):
-    if indegree[i] == 0:
-        heapq.heappush(heap, i)
+    for i in range(1, N + 1):
+        if indegree[i] == 0:
+            heapq.heappush(heap, i)
 
-answer = []
+    answer = []
 
-while heap:
-    cur = heapq.heappop(heap)
-    answer.append(cur)
-    for n in graph[cur]:
-        indegree[n] -= 1
-        if indegree[n] == 0:
-            heapq.heappush(heap, n)
+    while heap:
+        cur = heapq.heappop(heap)
+        answer.append(cur)
+        for n in graph[cur]:
+            indegree[n] -= 1
+            if indegree[n] == 0:
+                heapq.heappush(heap, n)
 
-print(*answer)
+    print(*answer)
+
+if __name__ == "__main__":
+    solve()
