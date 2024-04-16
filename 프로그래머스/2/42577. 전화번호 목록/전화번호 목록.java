61b0074043b6dcc363d607843a3e245f8e63d4cf
Hyeong Chan Kim <siler2474@gmail.com>
2024-04-16 17:26:05 +0900

    [level 2] Title: 전화번호 목록, Time: 273.60 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,21 +2,12 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Map<String, Boolean> map = new HashMap<String, Boolean>();
+        Arrays.sort(phone_book);
         
-        Arrays.sort(phone_book, (o1, o2) -> Integer.compare(o1.length(), o2.length()));
-    
-        for (String phone : phone_book) {
-            String tmp = "";
-            
-            for (int i = 0; i < phone.length(); i++) {
-                tmp += phone.charAt(i);
-                if (map.get(tmp) != null) {
-                    return false;
-                }
+        for (int i = 1; i < phone_book.length; i++) {
+            if (phone_book[i].startsWith(phone_book[i-1])) {
+                return false;
             }
-            
-            map.put(phone, true);
         }
         
         return true;
