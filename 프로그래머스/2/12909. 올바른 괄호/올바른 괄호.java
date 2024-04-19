fb2676f1640d51d19f31770f89a6bf5c9586416a
Hyeong Chan Kim <siler2474@gmail.com>
2024-04-19 16:49:46 +0900

    [level 2] Title: 올바른 괄호, Time: 17.29 ms, Memory: 63.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            }
+            else {
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
