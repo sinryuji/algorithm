ab637e81aca1c2887f6b8ebc1a3d2c247a52a244
Hyeong Chan Kim <siler2474@gmail.com>
2024-06-11 20:45:28 +0900

    [Gold V] Title: 암호 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,36 +1,20 @@
 import sys
+from itertools import combinations
 
 input = sys.stdin.readline
 
 
-def check(arr):
-    v, c = 0, 0
-    for i in arr:
-        if i in vowel:
-            v += 1
-        else:
-            c += 1
-    if v >= 1 and c >= 2:
-        return True
-    return False
-
-
-def dfs(start, arr):
-    if len(arr) == L:
-        if check(arr):
-            print(''.join(arr))
-        return
-
-    for i in range(start, C):
-        if arr[-1] < alphas[i]:
-            arr.append(alphas[i])
-            dfs(start + 1, arr)
-            arr.pop()
-
-
 L, C = map(int, input().split())
 alphas = sorted(list(input().split()))
-vowel = ['a', 'e', 'i', 'o', 'u']
+words = combinations(alphas, L)
 
-for i in range(C - L + 1):
-    dfs(i + 1, [alphas[i]])
+for word in words:
+    if len(word) != L:
+        continue
+    cnt = 0
+    for i in word:
+        if i in 'aeiou':
+            cnt += 1
+
+    if cnt >= 1 and len(word) - cnt >= 2:
+        print(''.join(word))
