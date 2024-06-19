92f254d15257a7bffe8de78c4319d751e5186736
Hyeong Chan Kim <siler2474@gmail.com>
2024-06-19 19:06:04 +0900

    [Gold IV] Title: 램프, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1034. 램프/램프.py
+++ 백준/Gold/1034. 램프/램프.py
@@ -3,17 +3,13 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-table = [list(input()) for _ in range(N)]
+table = [input() for _ in range(N)]
 K = int(input())
 
 answer = 0
 
 for row in range(N):
-    zero_cnt = 0
-    for num in table[row]:
-        if num == '0':
-            zero_cnt += 1
-
+    zero_cnt = table[row].count('0')
     if zero_cnt <= K and zero_cnt % 2 == K % 2:
         row_cnt = 0
         for row2 in range(N):
