6179e6e2adbf70ba85419a0d3ce7d0e4105c4976
Hyeong Chan Kim <siler2474@gmail.com>
2024-07-14 18:38:07 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 7024 ms, Memory: 617928 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
@@ -2,33 +2,42 @@ import sys
 
 input = sys.stdin.readline
 
-def solve():
-    n = int(input())
-    A, B, C, D = [], [], [], []
-    for _ in range(n):
-        a, b, c, d = map(int, input().split())
-        A.append(a)
-        B.append(b)
-        C.append(c)
-        D.append(d)
+n = int(input())
+A, B, C, D = [], [], [], []
+for _ in range(n):
+    a, b, c, d = map(int, input().split())
+    A.append(a)
+    B.append(b)
+    C.append(c)
+    D.append(d)
 
-    dict_ = dict()
-    answer = 0
+AB = []
+for a in A:
+    for b in B:
+        AB.append(a + b)
+AB.sort()
 
-    for a in A:
-        for b in B:
-            ab = a + b
-            if ab in dict_:
-                dict_[ab] += 1
-            else:
-                dict_[ab] = 1
+CD = []
+for c in C:
+    for d in D:
+        CD.append(c + d)
+CD.sort()
 
-    for c in C:
-        for d in D:
-            cd = -(c + d)
-            if cd in dict_:
-                answer += dict_[cd]
+answer = 0
+l, r = 0, len(CD) - 1
+while r >= 0 and l < len(AB):
+    s = AB[l] + CD[r]
+    if s < 0:
+        l += 1
+    elif s > 0:
+        r -= 1
+    else:
+        l_tmp = l
+        while l < len(AB) and AB[l_tmp] == AB[l]:
+            l += 1
+        r_tmp = r
+        while r >= 0 and CD[r_tmp] == CD[r]:
+            r -= 1
+        answer += (l - l_tmp) * (r_tmp - r)
 
-    print(answer)
-
-solve()
+print(answer)
