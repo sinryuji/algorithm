8fa4ec644165c1d5c5247c4a0cf395f50d949c32
Hyeong Chan Kim <siler2474@gmail.com>
2024-07-24 18:16:48 +0900

    [Gold I] Title: 외판원 순회, Time: 1176 ms, Memory: 46540 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.py
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.py
@@ -2,17 +2,12 @@ import sys
 
 input = sys.stdin.readline
 INF = 1e+9
-N = int(input())
-graph = []
-for i in range(N):
-    graph.append(list(map(int, input().split())))
-    
-dp = [[None for _ in range(1 << N)] for _ in range(N)]
-visited = 1
+
 
 def dfs(start, visited):
     if visited == (1 << N) - 1:
         if graph[start][0] != 0:
+            dp[start][visited] = graph[start][0]
             return graph[start][0]
         else:
             return INF
@@ -22,12 +17,16 @@ def dfs(start, visited):
 
     min_val = INF
     for i in range(1, N):
-        if (graph[start][i] != 0) and ((visited & (1 << i)) == 0):
-            min_val = min(min_val, dfs(i, visited | (1 << i)) + graph[start][i])
+        if graph[start][i] != 0 and (visited & (1 << i)) == 0:
+            min_val = min(min_val,
+                                 dfs(i, visited | (1 << i)) + graph[start][i])
     dp[start][visited] = min_val
 
     return min_val
 
 
-answer = dfs(0, visited)
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+dp = [[None] * (1 << N) for _ in range(N)]
+
 print(dfs(0, 1))
