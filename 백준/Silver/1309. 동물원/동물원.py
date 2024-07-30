f8c44aea4f89fdf683fd015b7609a02e0aceff87
Hyeong Chan Kim <siler2474@gmail.com>
2024-07-30 17:04:31 +0900

    [Silver I] Title: 동물원, Time: 72 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -1,20 +1,9 @@
 N = int(input())
-# dp = [0] * (N + 1)
+dp = [0] * (N + 1)
+dp[0] = 1
+dp[1] = 3
 
-if N == 1:
-    print(3)
-elif N == 2:
-    print(7)
-else:
-    # dp[1] = 3
-    # dp[2] = 7
-    pprev = 3
-    prev = 7
-    cur = 0
+for i in range(2, N + 1):
+    dp[i] = (dp[i-1] * 2 + dp[i-2]) % 9901
 
-    for i in range(3, N + 1):
-        cur = prev + prev + pprev
-        pprev = prev
-        prev = cur
-
-    print(cur % 9901)
+print(dp[N] % 9901)
