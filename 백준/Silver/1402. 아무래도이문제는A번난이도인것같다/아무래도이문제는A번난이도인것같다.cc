7f79b7a589d2a42d56bae252a00d4c328285a544
Hyeong Chan Kim <siler2474@gmail.com>
2024-08-03 20:17:28 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int t, a, b;
+    cin >> t;
+    
+    while (t--) {
+        cin >> a >> b;
+        cout << "yes" << endl;
+    }
+    
+    return 0;
+}
