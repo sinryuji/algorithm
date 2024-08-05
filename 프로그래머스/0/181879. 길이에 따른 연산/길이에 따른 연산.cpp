1dc49348638a17e428e6b6e0c24c71e86437f179
Hyeong Chan Kim <siler2474@gmail.com>
2024-08-05 18:01:32 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+int solution(vector<int> v) {
+    return v.size() >= 11 ? accumulate(v.begin(), v.end(), 0) : accumulate(v.begin(), v.end(), 1, multiplies<int>());
+}
