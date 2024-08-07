bb370b688e0307ae450ae0368fad87d0a7f24ea2
Hyeong Chan Kim <siler2474@gmail.com>
2024-08-07 17:09:20 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.10 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    
+    string str;
+    stringstream ss;
+    ss.str(my_string);
+    while (ss >> str)
+        answer.push_back(str);
+    
+    return answer;
+}
