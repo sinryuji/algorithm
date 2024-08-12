e4fe6940ed444edbdc67de0d8023e0dcad53fdf2
Hyeong Chan Kim <siler2474@gmail.com>
2024-08-12 15:25:38 +0900

    [Gold V] Title: 물병, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1052. 물병/물병.py
+++ 백준/Gold/1052. 물병/물병.py
@@ -2,7 +2,8 @@ N, K = map(int, input().split())
 
 answer = 0
 while bin(N).count('1') > K:
-    N += 1
-    answer += 1
-    
+    s = bin(N)[::-1].index('1')
+    N += 2 ** s
+    answer += 2 ** s
+
 print(answer)
