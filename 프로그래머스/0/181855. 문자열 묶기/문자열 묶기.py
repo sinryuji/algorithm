7029715f3d6f756250cdbada0e84a57c05d55baa
Hyeong Chan Kim <siler2474@gmail.com>
2024-08-19 23:52:52 +0900

    [level 0] Title: 문자열 묶기, Time: 17.27 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -1,9 +1,6 @@
 def solution(strArr):
     d = dict()
     for str in strArr:
-        if len(str) in d:
-            d[len(str)] += 1
-        else:
-            d[len(str)] = 1
+        d[len(str)] = d.get(len(str), 0) + 1
     
-    return sorted(d.items(), key=lambda x: x[1], reverse=True)[0][1]
+    return max(d.values())
