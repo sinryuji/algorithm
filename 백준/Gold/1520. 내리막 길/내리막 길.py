691d9339796e1c8b9f2886ed13afc2cd2ff55968
Hyeong Chan Kim <siler2474@gmail.com>
2024-09-09 16:30:46 +0900

    [Gold III] Title: 내리막 길, Time: 116 ms, Memory: 44136 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,25 +1,31 @@
 import sys
+
 input = sys.stdin.readline
 
+sys.setrecursionlimit(10 ** 6)
+
+
 def dfs(x, y):
-    if x == n - 1 and y == m - 1:
+    if x == N - 1 and y == M - 1:
         return 1
-    
+
     if dp[y][x] != -1:
         return dp[y][x]
-    
-    ways = 0
-    for i in range(4):
-        nx, ny = x + dx[i], y + dy[i]
-        if n > nx >= 0 and m > ny >= 0 and map[y][x] > map[ny][nx]:
-            ways += dfs(nx, ny)
-    dp[y][x] = ways
-    
+
+    way = 0
+    for dx, dy in d:
+        nx = x + dx
+        ny = y + dy
+        if 0 <= nx < N and 0 <= ny < M and board[ny][nx] < board[y][x]:
+            way += dfs(nx, ny)
+    dp[y][x] = way
+
     return dp[y][x]
 
-m, n = map(int, input().split())
-map = [list(map(int, input().split())) for _ in range(m)]
-dp = [[-1] * n for _ in range(m)]
-dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
+
+M, N = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(M)]
+dp = [[-1] * N for _ in range(M)]
+d = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 print(dfs(0, 0))
