46e0d5abc1b16a77926c48e57ad7fd36dbb84371
Hyeong Chan Kim <siler2474@gmail.com>
2024-09-28 15:28:32 +0900

    [level 2] Title: 시소 짝꿍, Time: 15.56 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -11,10 +11,10 @@ def solution(weights):
     weights = set(weights)
     for w in weights:
         if w * 2 / 3 in weights:
-            answer += counter[w * 2 // 3] * counter[w]
+            answer += counter[w * 2 / 3] * counter[w]
         if w * 2 / 4 in weights:
-            answer += counter[w * 2 // 4] * counter[w]
+            answer += counter[w * 2 / 4] * counter[w]
         if w * 3 / 4 in weights:
-            answer += counter[w * 3 // 4] * counter[w]
+            answer += counter[w * 3 / 4] * counter[w]
     
     return answer
