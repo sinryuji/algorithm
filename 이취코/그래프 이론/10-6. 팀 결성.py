c4793aacafb95dade4bc0e00894624712fdc6e59
sinryuji <siler2474@gmail.com>
2024-10-09 16:27:34 +0900

    팀 결성

---
+++ 이취코/그래프 이론/10-6. 팀 결성.py
@@ -0,0 +1,29 @@
+import sys
+
+input = sys.stdin.readline
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a, b):
+    a, b = find(a), find(b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+N, M = map(int, input().split())
+parent = [i for i in range(N + 1)]
+
+for _ in range(M):
+    op, a, b = map(int, input().split())
+    if op == 0:
+        union(a, b)
+    else:
+        if find(a) == find(b):
+            print('YES')
+        else:
+            print('NO')
