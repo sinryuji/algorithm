09d1bc2e69fd4fb1c380dc47868f9cac8f9f8546
Hyeong Chan Kim <siler2474@gmail.com>
2024-10-16 16:50:51 +0900

    [level 3] Title: 기둥과 보 설치, Time: 2799.51 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
@@ -0,0 +1,26 @@
+def possible(answer):
+    for x, y, a in answer:
+        if a == 0:
+            if y == 0 or [x - 1, y, 1] in answer or [x, y, 1] in answer or [x, y - 1, 0] in answer:
+                continue
+            return False
+        elif a == 1:
+            if [x, y - 1, 0] in answer or [x + 1, y - 1, 0] in answer or ([x - 1, y, 1] in answer and [x + 1, y, 1] in answer):
+                continue
+            return False
+    return True
+
+def solution(n, build_frame):
+    answer = []
+    
+    for x, y, a, b in build_frame:
+        if b == 0:
+            answer.remove([x, y, a])
+            if not possible(answer):
+                answer.append([x, y, a])
+        if b == 1:
+            answer.append([x, y, a])
+            if not possible(answer):
+                answer.pop()
+    
+    return sorted(answer)
