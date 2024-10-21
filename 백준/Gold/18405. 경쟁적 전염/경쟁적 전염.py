e74307a7b43a51bf5bdf59c68b4e3d1233ff1e92
Hyeong Chan Kim <siler2474@gmail.com>
2024-10-21 16:27:56 +0900

    [Gold V] Title: 경쟁적 전염, Time: 128 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,3 +1,5 @@
+# https://www.acmicpc.net/problem/18405
+
 import sys
 from collections import deque
 
@@ -7,22 +9,22 @@ N, K = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 S, X, Y = map(int, input().split())
 
-q = deque()
-for i in range(1, K + 1):
-    for y in range(N):
-        for x in range(N):
-            if board[y][x] == i:
-                q.append((x, y, 0))
+q = []
+for y in range(N):
+    for x in range(N):
+        if board[y][x] > 0:
+            q.append((board[y][x], x, y, 0))
+q = deque(sorted(q))
 
 d = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 while q:
-    x, y, sec = q.popleft()
+    i, x, y, sec = q.popleft()
     if sec == S:
         break
     for dx, dy in d:
         nx, ny = x + dx, y + dy
         if 0 <= nx < N and 0 <= ny < N and board[ny][nx] == 0:
-            board[ny][nx] = board[y][x]
-            q.append((nx, ny, sec + 1))
+            board[ny][nx] = i
+            q.append((i, nx, ny, sec + 1))
 
 print(board[X-1][Y-1])
