8a1dfee24aadc32864a8cc298916ca74cb74b519
Hyeong Chan Kim <siler2474@gmail.com>
2024-10-27 16:50:19 +0900

    [level 1] Title: 실패율, Time: 16.97 ms, Memory: 14.9 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,15 +1,12 @@
 def solution(N, stages):
     arr = []
-    for i in range(1, N+1):
-        challenge = 0
-        over = 0
-        for j in stages:
-            if j == i:
-                challenge += 1
-            if j >= i:
-                over += 1
-        arr.append((challenge / over if over > 0 else 0, i))
-    arr.sort(key=lambda x: (-x[0]))
+    cnt = [0] * (N + 2)
+    for stage in stages:
+        cnt[stage] += 1
     
-    return [i[1] for i in arr]
-        
+    for i in range(1, N + 1):
+        over = sum(cnt[i:])
+        challenge = cnt[i]
+        arr.append((i, 0 if over == 0 else challenge / over))
+    
+    return list(i[0] for i in sorted(arr, key=lambda x: -x[1]))
