75a741876c58e5d1b81d675eee29144ed0859500
Hyeong Chan Kim <siler2474@gmail.com>
2024-10-30 17:28:58 +0900

    [level 4] Title: 가사 검색, Time: 135.09 ms, Memory: 30.5 MB -BaekjoonHub

---
+++ 프로그래머스/4/60060. 가사 검색/가사 검색.py
@@ -0,0 +1,29 @@
+from bisect import bisect_left, bisect_right
+
+def count_by_range(arr, left, right):
+    right_idx = bisect_right(arr, right)
+    left_idx = bisect_left(arr, left)
+    return right_idx - left_idx
+
+array = [[] for _ in range(10001)]
+reversed_array = [[] for _ in range(10001)]
+
+def solution(words, queries):
+    answer = []
+    
+    for word in words:
+        array[len(word)].append(word)
+        reversed_array[len(word)].append(word[::-1])
+        
+    for i in range(10001):
+        array[i].sort()
+        reversed_array[i].sort()
+        
+    for q in queries:
+        if q[0] != '?':
+            res = count_by_range(array[len(q)], q.replace('?', 'a'), q.replace('?', 'z'))
+        else:
+            res = count_by_range(reversed_array[len(q)], q[::-1].replace('?', 'a'), q[::-1].replace('?', 'z'))
+        answer.append(res)
+    
+    return answer
