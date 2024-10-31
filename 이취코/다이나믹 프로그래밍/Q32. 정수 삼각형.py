2b1228ae60227c044750a094e66931831c137a26
sinryuji <siler2474@gmail.com>
2024-10-31 16:38:17 +0900

    DP) Q32 정수 삼각형

---
+++ 이취코/다이나믹 프로그래밍/Q32. 정수 삼각형.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/1932
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+dp = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(1, n):
+    for j in range(i+1):
+        if j == 0:
+            dp[i][j] += dp[i-1][j]
+        elif j == i:
+            dp[i][j] += dp[i-1][j-1]
+        else:
+            dp[i][j] = dp[i][j] + max(dp[i-1][j-1], dp[i-1][j])
+
+print(max(dp[-1]))
