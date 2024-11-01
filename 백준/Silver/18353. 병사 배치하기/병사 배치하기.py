7c899e40b5254ae0018846cc2815b2581c79d4e6
Hyeong Chan Kim <siler2474@gmail.com>
2024-11-01 18:45:33 +0900

    [Silver II] Title: 병사 배치하기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -2,13 +2,27 @@ import sys
 
 input = sys.stdin.readline
 
+
+def bs(array, target):
+    left, right = 0, len(array) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if array[mid] > target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return left
+
+
 N = int(input())
 arr = list(map(int, input().split()))
-dp = [1] * N
+s = [arr[0]]
 
-for i in range(N):
-    for j in range(i):
-        if arr[j] > arr[i]:
-            dp[i] = max(dp[i], dp[j] + 1)
-            
-print(N - max(dp))
+for i in arr[1:]:
+    if i < s[-1]:
+        s.append(i)
+    else:
+        s[bs(s, i)] = i
+
+print(N - len(s))
