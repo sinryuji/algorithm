19a48548c51f6dbade757253b4a439d161ca5000
sinryuji <siler2474@gmail.com>
2024-11-18 18:20:00 +0900

    순열과 조합) B-2 암호 만들기

---
+++ 이취코/기타 알고리즘/순열과 조합/B-2. 암호 만들기.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/1759
+
+from itertools import combinations
+
+def check(secret):
+    cons, vowel = 0, 0
+    vowel_list = ['a', 'e', 'i', 'o', 'u']
+    for c in secret:
+        if c in vowel_list:
+            vowel += 1
+        else:
+            cons += 1
+        if cons >= 2 and vowel >= 1:
+            return True
+    return False
+
+L, C = map(int, input().split())
+letters = sorted(list(input().split()))
+
+for secret in list(combinations(letters, L)):
+    if check(secret):
+        print(''.join(secret))
