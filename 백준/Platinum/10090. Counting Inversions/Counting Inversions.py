e355a51053464b3c5af17f12dbb08bb8db2eda2b
Hyeong Chan Kim <siler2474@gmail.com>
2024-12-18 18:30:17 +0900

    [Platinum V] Title: Counting Inversions, Time: 2236 ms, Memory: 237160 KB -BaekjoonHub

--- 백준/Platinum/10090. Counting Inversions/Counting Inversions.py
+++ 백준/Platinum/10090. Counting Inversions/Counting Inversions.py
@@ -3,28 +3,37 @@ import sys
 input = sys.stdin.readline
 
 
-def prefix_sum(i):
+def query(l, r):
     s = 0
-    while i > 0:
-        s += tree[i]
-        i -= i & -i
+    while l < r:
+        if l % 2:
+            s += tree[l]
+            l += 1
+        if r % 2:
+            r -= 1
+            s += tree[r]
+        l //= 2
+        r //= 2
     return s
 
 
-def update(i):
-    while i <= N:
-        tree[i] += 1
-        i += i & -i
+def update(idx, val):
+    tree[idx] += val
+    while idx > 1:
+        idx //= 2
+        tree[idx] = tree[idx * 2] + tree[idx * 2 + 1]
 
 
-N = int(input())
-nums = list(map(int, input().split()))
-arr = sorted([(-v, i+1) for i, v in enumerate(nums)])
-tree = [0] * (N + 1)
+n = int(input())
+seq = list(map(int, input().split()))
+size = 1 << (n - 1).bit_length()
+tree = [0] * (size * 2)
+arr = [(val, i) for i, val in enumerate(seq)]
+arr.sort(reverse=True)
 
 answer = 0
-for v, i in arr:
-    answer += prefix_sum(i)
-    update(i)
+for val, i in arr:
+    answer += query(size, i + size)
+    update(i + size, 1)
 
 print(answer)
