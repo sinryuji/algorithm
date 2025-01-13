b747df9060438f4e72b92aa51a8f436185090770
Hyeong Chan Kim <siler2474@gmail.com>
2025-01-13 16:47:07 +0900

    Time: 73 ms (78.10%) | Memory: 18.8 MB (52.38%) - LeetSync

--- leetcode/3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
+++ leetcode/3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -1,3 +1,8 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
-        return sum(1 if v & 1 else 2 for v in Counter(s).values())
+        res = 0
+        for i in range(ord('a'), ord('z') + 1):
+            cnt = s.count(chr(i))
+            if cnt & 1: res += 1
+            elif cnt != 0: res += 2
+        return res
