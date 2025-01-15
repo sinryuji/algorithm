acf20dc1f143b1aeb31b661091b69f11f82c3a83
sinryuji <siler2474@gmail.com>
2025-01-15 18:15:46 +0900

    [level 2] Title: 순위 검색, Time: 4034.08 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.py
@@ -1,59 +1,34 @@
 from collections import defaultdict
 
-def bisect_left(arr, target):
-    left, right = 0, len(arr)
-    while left < right:
-        mid = (left + right) // 2
-        if arr[mid] < target:
-            left = mid + 1
+def bs(arr, target):
+    l, r = 0, len(arr)
+    while l < r:
+        m = (l + r) // 2
+        if arr[m] < target:
+            l = m + 1
         else:
-            right = mid
-    return left
-
+            r = m
+    return l
 
 def solution(info, query):
     answer = []
-
-    d = {'java': 'a',
-         'cpp': 'b',
-         'python': 'c',
-         'backend': 'a',
-         'frontend': 'b',
-         'junior': 'a',
-         'senior': 'b',
-         'chicken': 'a',
-         'pizza': 'b'
-         }
-
-    applicants = defaultdict(list)
+    data = defaultdict(list)
+    
     for i in info:
-        x, y, z, w, s = i.split()
-        applicants[d[x] + d[y] + d[z] + d[w]].append(int(s))
-
-    for key in applicants.keys():
-        applicants[key].sort()
-
-    def dfs(depth, key):
-        nonlocal cnt, s
-        if depth == 4:
-            arr = applicants[key]
-            cnt += len(arr) - bisect_left(arr, s)
-            return
-
-        if q[depth] == '-':
-            dfs(depth + 1, key + 'a')
-            dfs(depth + 1, key + 'b')
-            if depth == 0:
-                dfs(depth + 1, key + 'c')
-        else:
-            dfs(depth + 1, key + d[q[depth]])
-
+        i = i.split()
+        for a in [i[0], '-']:
+            for b in [i[1], '-']:
+                for c in [i[2], '-']:
+                    for d in [i[3], '-']:
+                        data[(a, b, c, d)].append(int(i[4]))
+                        
+    for k in data.keys():
+        data[k].sort()
+        
     for q in query:
-        cnt = 0
-        x, y, z, w = q.split(' and ')
-        f, s = w.split()
-        q, s = [x, y, z, f], int(s)
-        dfs(0, '')
-        answer.append(cnt)
+        q = q.split()
+        arr = data[(q[0], q[2], q[4], q[6])]
+        s = int(q[7])
+        answer.append(len(arr) - bs(arr, s))
 
     return answer
