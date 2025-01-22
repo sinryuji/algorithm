9e1fbef87e5e32d10340dba3e3584b7f9d9a93a2
Hyeong Chan Kim <siler2474@gmail.com>
2025-01-22 16:25:53 +0900

    [Gold I] Title: 냅색문제, Time: 84 ms, Memory: 36796 KB -BaekjoonHub

--- 백준/Gold/1450. 냅색문제/냅색문제.py
+++ 백준/Gold/1450. 냅색문제/냅색문제.py
@@ -1,44 +1,31 @@
 import sys
 from itertools import combinations
+from bisect import bisect_right
 
 input = sys.stdin.readline
 
 N, C = map(int, input().split())
 arr = list(map(int, input().split()))
 
-arr_1 = arr[:N//2]
-arr_2 = arr[N//2:]
+arr_left = arr[:N // 2]
+arr_right = arr[N // 2:]
 
-subsum_a = []
-subsum_b = []
+sum_left = []
+sum_right = []
 
-for i in range(len(arr_1) + 1):
-    comb_a = combinations(arr_1, i)
-    for comb in comb_a:
-        subsum_a.append(sum(comb))
+for i in range(len(arr_left) + 1):
+    for comb in combinations(arr_left, i):
+        sum_left.append(sum(comb))
 
-for i in range(len(arr_2) + 1):
-    comb_b = combinations(arr_2, i)
-    for comb in comb_b:
-        subsum_b.append(sum(comb))
+for i in range(len(arr_right) + 1):
+    for comb in combinations(arr_right, i):
+        sum_right.append(sum(comb))
 
-subsum_a.sort()
+sum_left.sort()
 answer = 0
-
-for element_b in subsum_b:
-    if element_b > C:
+for s in sum_right:
+    if s > C:
         continue
-
-    start = 0
-    end = len(subsum_a) - 1
-
-    while start <= end:
-        mid = (start + end) // 2
-        if subsum_a[mid] + element_b > C:
-            end = mid - 1
-        else:
-            start = mid + 1
-
-    answer += end + 1
+    answer += bisect_right(sum_left, C - s)
 
 print(answer)
