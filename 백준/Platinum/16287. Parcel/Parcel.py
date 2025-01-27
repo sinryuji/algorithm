23f2650f5ca295ba320919e8b4bcc68b186c3780
Hyeong Chan Kim <siler2474@gmail.com>
2025-01-27 18:20:12 +0900

    [Platinum V] Title: Parcel, Time: 1880 ms, Memory: 76548 KB -BaekjoonHub

--- 백준/Platinum/16287. Parcel/Parcel.py
+++ 백준/Platinum/16287. Parcel/Parcel.py
@@ -2,20 +2,28 @@ import sys
 
 input = sys.stdin.readline
 
+
 def solve():
+    dp = [0] * (w + 1)
+    half = w // 2
+
     for i in range(n):
         for j in range(i + 1, n):
-            if (val := arr[i] + arr[j]) <= w:
-                visited[val] = (i, j)
-    for i in range(n):
-        for j in range(i + 1, n):
-            val = arr[i] + arr[j]
-            if val <= w and visited[w - val] and i not in visited[w - val] and j not in visited[w - val]:
-                return True
+            v = arr[i] + arr[j]
+
+            if v < half:
+                if not dp[v]:
+                    dp[v] = (i, j)
+            else:
+                v = w - v
+                if v > 0 and dp[v]:
+                    a, b = dp[v]
+                    if i != a and i != b and j != a and j != b:
+                        return True
     return False
 
+
 w, n = map(int, input().split())
-arr = list(map(int, input().split()))
-visited = [0] * (w + 1)
+arr = sorted(list(map(int, input().split())))
 
 print('YES' if solve() else 'NO')
