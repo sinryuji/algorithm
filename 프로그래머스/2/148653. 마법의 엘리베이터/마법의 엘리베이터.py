ca4365815e76816cca0e72073d68cc50b2e5efa7
Hyeong Chan Kim <siler2474@gmail.com>
2025-01-31 16:39:09 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -2,4 +2,4 @@ def solution(storey):
     if storey < 10:
         return min(storey, 11 - storey)
     left = storey % 10
-    return left + solution(storey // 10) if left < 5 or (left == 5 and (storey // 10) % 10 < 5) else 10 - left + solution(storey // 10 + 1)
+    return min(left + solution(storey // 10), 10 - left + solution(storey // 10 + 1))
