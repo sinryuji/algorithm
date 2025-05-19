a5bb7639de0d6a8a5f1ed681346a3e5a949757dc
Hyeong Chan Kim <siler2474@gmail.com>
2025-05-19 17:11:23 +0900

    [Gold V] Title: 동전, Time: 76 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -8,14 +8,12 @@ for _ in range(T):
     coins = [0] + list(map(int, input().split()))
     M = int(input())
 
-    dp = [[0] * (M + 1) for _ in range(N + 1)]
-    for i in range(N + 1):
-        dp[i][0] = 1
+    dp = [0] * (M + 1)
+    dp[0] = 1
 
-    for i in range(1, N + 1):
-        for j in range(1, M + 1):
-            dp[i][j] = dp[i - 1][j]
-            if j - coins[i] >= 0:
-                dp[i][j] += dp[i][j - coins[i]]
+    for coin in coins:
+        for i in range(1, M + 1):
+            if coin <= i:
+                dp[i] += dp[i - coin]
 
-    print(dp[N][M])
+    print(dp[M])
