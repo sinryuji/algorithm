1ad9037c909557a2bfa93c94e6176ffda553e664
Hyeong Chan Kim <siler2474@gmail.com>
2025-05-27 16:27:36 +0900

    [Gold V] Title: 극장 좌석, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2302. 극장 좌석/극장 좌석.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+vips = set(int(input()) for _ in range(M))
+
+dp = [0] * (N + 1)
+dp[1] = 1
+if N > 1:
+    if 1 in vips or 2 in vips:
+        dp[2] = 1
+    else:
+        dp[2] = 2
+for i in range(3, N + 1):
+    if i in vips or i - 1 in vips:
+        dp[i] = dp[i - 1]
+    else:
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+print(dp[N])
