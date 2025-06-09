697124548b152d8ad9ae25dfed56126586a6d2ee
Hyeong Chan Kim <siler2474@gmail.com>
2025-06-09 17:31:44 +0900

    [level 1] Title: 유연근무제, Time: 2.04 ms, Memory: 9.62 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -0,0 +1,21 @@
+from collections import deque
+import itertools
+
+def solution(schedules, timelogs, startday):
+    answer = 0
+    
+    for log, sche in zip(timelogs, schedules):
+        log = deque(log)
+        log.rotate(startday - 1)
+        
+        succeed = True
+        sche = (sche // 100 * 60) + sche % 100
+        for time in itertools.islice(log, 5):
+            time = (time // 100 * 60) + time % 100
+            if time - sche > 10:
+                succeed = False
+                
+        if succeed:
+            answer += 1
+            
+    return answer
