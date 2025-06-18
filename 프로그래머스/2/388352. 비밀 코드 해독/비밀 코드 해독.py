62991188d5fbee2e57ffa5bab70d290af5869557
Hyeong Chan Kim <siler2474@gmail.com>
2025-06-18 17:21:19 +0900

    [level 2] Title: 비밀 코드 해독, Time: 180.93 ms, Memory: 21.8 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -1,19 +1,9 @@
 from itertools import combinations
 
-def solution(n, q, ans):
-    answer = 0
-    lst = [i for i in range(1, n + 1)]
-    m = len(ans)
+def solution(n, qs, ans):
+    combi = list(combinations(range(1, n + 1), 5))
     
-    for c in combinations(lst, 5):
-        for i in range(m):
-            cnt = 0
-            for j in q[i]:
-                if j in c:
-                    cnt += 1
-            if cnt != ans[i]:
-                break
-        else:
-            answer += 1
-                
-    return answer
+    for q, cnt in zip(qs, ans):
+        combi = [c for c in combi if len(set(c) & set(q)) == cnt]
+    
+    return len(combi)
