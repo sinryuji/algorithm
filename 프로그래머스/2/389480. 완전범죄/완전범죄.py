1f166b3f91fa5b3aafc4c80a1ac8f21b8cf3c321
Hyeong Chan Kim <siler2474@gmail.com>
2025-06-23 18:25:06 +0900

    [level 2] Title: 완전범죄, Time: 18.17 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -2,7 +2,7 @@ def solution(info, n, m):
     info_len = len(info)
     dp = [[[False] * m for _ in range(n)] for _ in range(info_len + 1)]
     dp[0][0][0] = True
-    
+
     for i, (info_a, info_b) in enumerate(info):
         for a in range(n):
             for b in range(m):
@@ -13,10 +13,10 @@ def solution(info, n, m):
                         dp[i+1][na][b] = True
                     if nb < m:
                         dp[i+1][a][nb] = True
-                        
+
     for a in range(n):
         for b in range(m):
             if dp[info_len][a][b]:
                 return a
-    
+
     return -1
