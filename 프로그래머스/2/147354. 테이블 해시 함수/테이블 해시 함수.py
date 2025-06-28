092149ae65f7c39d55bccd8636baf379f540027f
Hyeong Chan Kim <siler2474@gmail.com>
2025-06-28 17:39:35 +0900

    [level 2] Title: 테이블 해시 함수, Time: 56.13 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -1,14 +1,6 @@
-def solution(data, col, row_begin, row_end):
-    answer = 0
+from functools import reduce
 
+def solution(data, col, row_begin, row_end):
     data.sort(key=lambda x: (x[col - 1], -x[0]))
 
-    s = []
-    for i in range(row_begin - 1, row_end):
-        s.append(sum(n % (i+1) for n in data[i]))
-
-    answer = s[0]
-    for n in s[1:]:
-        answer ^= n
-
-    return answer
+    return reduce(lambda x, y: x ^ y, [sum(x % (i+1) for x in data[i]) for i in range(row_begin-1, row_end)])
