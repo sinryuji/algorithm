68be56e1016f45d7d99164b28f22dcdbee8e4d5b
Hyeong Chan Kim <siler2474@gmail.com>
2025-06-28 17:04:05 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 48 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
@@ -1,10 +1,14 @@
+import heapq
+
 def solve():
     n, m = map(int, input().split())
-    cards = sorted(list(map(int, input().split())))
+    cards = list(map(int, input().split()))
+    heapq.heapify(cards)
 
     for _ in range(m):
-        cards[0] = cards[1] = cards[0] + cards[1]
-        cards.sort()
+        a, b = heapq.heappop(cards), heapq.heappop(cards)
+        heapq.heappush(cards, a + b)
+        heapq.heappush(cards, a + b)
 
     print(sum(cards))
 
