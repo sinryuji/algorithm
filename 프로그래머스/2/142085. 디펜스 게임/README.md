c81025e7cf0545f660f2e0a76409d249318d95ae
Hyeong Chan Kim <siler2474@gmail.com>
2025-07-12 22:12:27 +0900

    [level 2] Title: 디펜스 게임, Time: 162.83 ms, Memory: 44.7 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/README.md
+++ 프로그래머스/2/142085. 디펜스 게임/README.md
@@ -1,10 +1,10 @@
 # [level 2] 디펜스 게임 - 142085 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085) 
 
 ### 성능 요약
 
-메모리: 45 MB, 시간: 722.68 ms
+메모리: 44.7 MB, 시간: 162.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 12일 22:08:17
+2025년 07월 12일 22:12:23
 
 ### 문제 설명
 
--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
@@ -1,17 +1,10 @@
 import heapq
 
 def solution(n, k, enemy):
-    stage = len(enemy)
-    if k >= stage:
-        return stage
-    q = []
-    
-    for i in range(stage):
-        heapq.heappush(q, enemy[i])
-        if len(q) > k:
-            last = heapq.heappop(q)
-            if last > n:
-                return i
-            n -= last
-    
-    return stage
+    q = enemy[:k]
+    heapq.heapify(q)
+    for i in range(k, len(enemy)):
+        n -= heapq.heappushpop(q, enemy[i])
+        if n < 0:
+            return i
+    return len(enemy)
