d4aa115b9842ebee7c05845fcf722969173c1d81
Hyeong Chan Kim <siler2474@gmail.com>
2025-08-15 17:50:05 +0900

    [level 3] Title: 베스트앨범, Time: 0.11 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -5,12 +5,10 @@ def solution(genres, plays):
     for i in range(len(genres)):
         d.setdefault(genres[i], []).append((i, plays[i]))
         
-    for k in d.keys():
-        d[k].sort(key=lambda x: (x[1], -x[0]))
-        
     sorted_keys = sorted(d.keys(), key=lambda k: sum(v[1] for v in d[k]), reverse=True)
     
     for k in sorted_keys:
+        d[k].sort(key=lambda x: (x[1], -x[0]))
         for _ in range(2):
             if d[k]:
                 i, v = d[k].pop()
