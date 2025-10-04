2952ee141e68b5c0d87ede2005cbdd9e8ce5752e
Hyeong Chan Kim <siler2474@gmail.com>
2025-10-04 19:07:16 +0900

    [Gold IV] Title: 치즈, Time: 76 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -10,6 +10,7 @@ def bfs(arr):
     visited = [[False] * M for _ in range(N)]
     visited[0][0] = True
     q = deque([(0, 0)])
+    remove = set()
     while q:
         r, c = q.popleft()
         for dr, dc in d:
@@ -18,33 +19,24 @@ def bfs(arr):
             if 0 <= nr < N and 0 <= nc < M and not visited[nr][nc]:
                 if arr[nr][nc] == 0:
                     q.append((nr, nc))
-                if arr[nr][nc] == 1:
-                    arr[nr][nc] = -1
+                elif (nr, nc) not in remove:
+                    remove.add((nr, nc))
                 visited[nr][nc] = True
-
-
-def cleanup(arr):
-    removed = 0
-    for r in range(N):
-        for c in range(M):
-            if arr[r][c] == -1:
-                arr[r][c] = 0
-                removed += 1
-    return removed
-
+    return remove
 
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 
 ans = 0
-prev = 0
+remove_cnt = 0
 while True:
-    bfs(board)
-    removed = cleanup(board)
-    if not removed:
+    remove = bfs(board)
+    if not remove:
         break
+    for r, c in remove:
+        board[r][c] = 0
     ans += 1
-    prev = removed
+    remove_cnt = len(remove)
 
 print(ans)
-print(prev)
+print(remove_cnt)
