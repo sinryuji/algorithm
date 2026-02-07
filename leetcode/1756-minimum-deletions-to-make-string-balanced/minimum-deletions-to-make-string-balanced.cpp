51b5a07c3cbd7380dd70e55cb53264e5400bfc12
Hyeong Chan Kim <siler2474@gmail.com>
2026-02-07 15:52:15 +0900

    Time: 10 ms (92.46%) | Memory: 25.6 MB (72.34%) - LeetSync

--- leetcode/1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ leetcode/1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n = s.length();
-        int min_deletions = 0;
-        int b_count = 0;
+        int deletions = 0;
+        int bCount = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (s[i] == 'b') b_count++;
-            else min_deletions = min(min_deletions + 1, b_count);
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'b') bCount++;
+            else if (bCount > 0) {
+                bCount--;
+                deletions++;
+            }
         }
 
-        return min_deletions;
+        return deletions;
     }
 };
