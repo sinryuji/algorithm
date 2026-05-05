4a674546fa146f220937b0d34d9dbc753888334a
Hyeong Chan Kim <siler2474@gmail.com>
2026-05-05 17:15:58 +0900

    Time: 0 ms (100.00%) | Memory: 8.2 MB (62.30%) - LeetSync

---
+++ leetcode/812-rotate-string/rotate-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.size() != goal.size())
+            return false;
+        for (int i = 0; i < s.size(); i++) {
+            rotate(s.begin(), s.begin() + 1, s.end());
+            if (s == goal)
+                return true;
+        }
+        return false;
+    }
+};
